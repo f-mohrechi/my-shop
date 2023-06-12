058022b099206a7f04711d47363d76109d2cbbae
mahi <f.mohrechi78@gmail.com>
2023-06-12 12:49:02 +0330

    add tailwindCSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'dark': '#264653',
+        'light': '#2a9d8f',
+        'yellow': '#e9c46a',
+        'orange': '#f4a261',
+        'orange-2': '#e76f51',
+        'fff': '#f7f7f7',
       },
+      fontFamily: {
+        'popins' : ['Poppins', 'sans-serif']
+      }
+    },
+    screens:{
+      'xs': '350px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [],
